5765b66e1725d20c7304ce471eb739e45deda20e
Pedro Henrique Migliatti <pedro.migliatti@gmail.com>
2018-11-12 14:41:16 -0200

    Add the cover-art-archive schema

--- create-database.sh
+++ create-database.sh
@@ -14,17 +14,22 @@ psql -h postgresql -d musicbrainz -U $POSTGRES_USER -a -f Extensions.sql
 rm Extensions.sql
 
 wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateTables.sql
+wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/962c5c01e64988b8197eb6ea1325bec6af69ff7f/admin/sql/updates/20121015-caa-as-of-schema-15.sql
 psql -h postgresql -d musicbrainz -U $POSTGRES_USER -a -f CreateTables.sql
+psql -h postgresql -d musicbrainz -U $POSTGRES_USER -a -f 20121015-caa-as-of-schema-15.sql
 rm CreateTables.sql
+rm 20121015-caa-as-of-schema-15.sql
 
 echo "Downloading last Musicbrainz dump"
 wget -nd -nH -P /tmp http://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST
 LATEST="$(cat /tmp/LATEST)"
 wget -nd -nH -P /tmp http://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/$LATEST/mbdump-derived.tar.bz2
 wget -nd -nH -P /tmp http://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/$LATEST/mbdump.tar.bz2
-
+wget -nd -nH -P /tmp http://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/20181110-001544/mbdump-cover-art-archive.tar.bz2
 
 echo "Uncompressing Musicbrainz dump"
+tar xjf /tmp/mbdump-cover-art-archive.tar.bz2
+rm mbdump-cover-art-archive.tar.bz2
 tar xjf /tmp/mbdump-derived.tar.bz2
 rm mbdump-derived.tar.bz2
 tar xjf /tmp/mbdump.tar.bz2
